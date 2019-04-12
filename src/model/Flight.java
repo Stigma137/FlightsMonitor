837be0c5d9ec599f4a46c5cd6716cb45425d6c4b
Camilo Enriquez <rot7447@gmail.com>
2019-04-11 23:27:11 -0500

    It's over

--- src/model/Flight.java
+++ src/model/Flight.java
@@ -1,6 +1,8 @@
 package model;
 
-public class Flight {
+
+public class Flight implements Comparable<Flight>{
+	//Attributes
 	private String airLine;
 	private String date;
 	private String time;
@@ -8,6 +10,14 @@ public class Flight {
 	private int flightNumber;
 	private int boardingGate;
 	
+	/* Method that allows to create an instance of an object of Flight type.
+	 * @param a String that represents the flight airline.
+	 * @param a String that represents the flight date.
+	 * @param a String that represents the flight time.
+	 * @param a String that represents the flight destination.
+	 * @param an integer that indicates the flight number id.
+	 * @param an integer that indicates the flight number boarding gate.
+	 */
 	public Flight(String airLine, String date, String time, String destination, int flightNumber, int boardingGate) {
 		this.airLine = airLine;
 		this.boardingGate = boardingGate;
@@ -16,53 +26,93 @@ public class Flight {
 		this.flightNumber = flightNumber;
 		this.time = time;
 	}
-
+	/* Method that allows to obtain a String that represents the flight airline.
+	 * @return a String that represents the flight airline.
+	 */
 	public String getAirLine() {
 		return airLine;
 	}
-
+	/* Method that allows to set a String that represents the flight airline.
+	 * @param a String that represents the flight airline.
+	 */
 	public void setAirLine(String airLine) {
 		this.airLine = airLine;
 	}
-
+	/* Method that allows to obtain a String that represents the flight date.
+	 * @return a String that represents the flight date.
+	 */
 	public String getDate() {
 		return date;
 	}
-
+	/* Method that allows to set a String that represents the flight date.
+	 * @param a String that represents the flight date.
+	 */
 	public void setDate(String date) {
 		this.date = date;
 	}
-
+	/* Method that allows to obtain a String that represents the flight time.
+	 * @return a String that represents the flight airline.
+	 */
 	public String getTime() {
 		return time;
 	}
-
+	/* Method that allows to set a String that represents the flight time.
+	 * @param a String that represents the flight airline.
+	 */
 	public void setTime(String time) {
 		this.time = time;
 	}
-
+	/* Method that allows to obtain a String that represents the flight destination.
+	 * @return a String that represents the flight destination.
+	 */
 	public String getDestination() {
 		return destination;
 	}
-
+	/* Method that allows to obtain a String that represents the flight destination.
+	 * @param a String that represents the flight destination.
+	 */
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
-
+	/* Method that allows to obtain an integer that indicates the flight number id.
+	 * @return an integer that indicates the flight number id.
+	 */
 	public int getFlightNumber() {
 		return flightNumber;
 	}
-
+	/* Method that allows to set an integer that indicates the flight number id.
+	 * @param an integer that indicates the flight number id.
+	 */
 	public void setFlightNumber(int flightNumber) {
 		this.flightNumber = flightNumber;
 	}
-
+	/* Method that allows to obtain an integer that indicates the flight number boarding gate.
+	 * @return an integer that indicates the flight number boarding gate.
+	 */
 	public int getBoardingGate() {
 		return boardingGate;
 	}
-
+	/* Method that allows to set an integer that indicates the flight number boarding gate.
+	 * @param an integer that indicates the flight number boarding gate.
+	 */
 	public void setBoardingGate(int boardingGate) {
 		this.boardingGate = boardingGate;
 	}
-	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Airline = " + airLine + ", Date = " + date + ", Time = " + time + ", Destination = " + destination
+				+ ", Flight Number = " + flightNumber + ", Boarding Gate = " + boardingGate;
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Flight f) {
+		return this.date.compareTo(f.date);
+	}
 }
--- src/ui/FlightsMonitorController.java
+++ src/ui/FlightsMonitorController.java
@@ -1,5 +1,6 @@
 package ui;
 
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,22 +10,30 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import model.Flight;
 import model.FlightsMonitor;
 
 public class FlightsMonitorController implements Initializable{
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button forwardButton;
     @FXML
     private TextField searchText;
     @FXML
     private TextField flightsNumberText;
-    @FXML
-    private TitledPane sortT;
     @FXML
     private RadioButton airLineSort;
     @FXML
@@ -38,7 +47,7 @@ public class FlightsMonitorController implements Initializable{
     @FXML
     private RadioButton timeSort;
     @FXML
-    private TitledPane searchT;
+    private RadioButton datetimeSort;
     @FXML
     private RadioButton airLineSearch;
     @FXML
@@ -65,24 +74,142 @@ public class FlightsMonitorController implements Initializable{
     private TableColumn<Flight, Integer> flightT;
     @FXML
     private TableColumn<Flight, String> timeT;
+    @FXML
+    private ToggleGroup  sortGroup;
+    @FXML
+    private ToggleGroup  searchGroup;
+    
     private FlightsMonitor fm;
+    private final static int ITEMS_PER_PAGE = 23;
+    private int currentPage;
     private ObservableList<Flight> flights;
+    
+    /* Event that allows to generate a determinate number of flights.
+     * @param an ActionEvent that represents the generate button.
+     */
     @FXML
     void generateFlights(ActionEvent event) throws NumberFormatException, IOException {
-    	flightsTable.setItems(fm.generateFlights(Integer.parseInt(flightsNumberText.getText())));
+    	fm.generateFlights(Integer.parseInt(flightsNumberText.getText()));
+    	flights = fm.getFlights();
+    	setupPage();
     }
+    /* Event that allows to search a flight in the flights list by a determinate criterion.
+     * @param an ActionEvent that represents the search button.
+     */
     @FXML
     void searchBy(ActionEvent event) {
-
+    	String option = (String) (searchGroup.getSelectedToggle().getUserData());
+    	Pane root = new Pane();
+		Label result = new Label();
+		result.relocate(5, 25);
+		root.getChildren().addAll(result);
+		Scene scene = new Scene(root, 850, 60);
+		Stage stage = new Stage();
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.setTitle("SEARCHING RESULT");
+		stage.show();
+    	switch(option) {
+    	case "airsearch":
+    		result.setText(fm.search("airsearch", searchText.getText()).toString());
+    		break;
+    	case "boardsearch":
+    		result.setText(fm.search("boardsearch", searchText.getText()).toString());
+    		break;
+    	case "datesearch":
+    		result.setText(fm.search("datesearch", searchText.getText()).toString());
+    		break;
+    	case "destinationsearch":
+    		result.setText(fm.search("destinationsearch", searchText.getText()).toString());
+    		break;
+    	case "flightsearch":
+    		result.setText(fm.search("flightsearch", searchText.getText()).toString());
+    		break;
+    	case "timesearch":
+    		result.setText(fm.search("timesearch", searchText.getText()).toString());
+    		break;
+    		
+    	}
     }
-
+    /* Event that allows to sort the flights list by a determinate criterion.
+     * @param an ActionEvent that represents the sort button.
+     */
     @FXML
     void sortBy(ActionEvent event) {
-
+    	String option = (String) sortGroup.getSelectedToggle().getUserData();
+    	fm.sort(option);
+    	switch(option) {
+    	case "airlinesort":
+    		fm.sort("airlinesort");
+    		setupPage();
+    		break;
+    	case "boardsort":
+    		fm.sort("boardsort");
+    		setupPage();
+    		break;
+    	case "datesort":
+    		fm.sort("datesort");
+    		setupPage();
+    		break;
+    	case "dessort":
+    		fm.sort("dessort");
+    		setupPage();
+    		break;
+    	case "flightsort":
+    		fm.sort("flightsort");
+    		setupPage();
+    		break;
+    	case "timesort":
+    		fm.sort("timesort");
+    		setupPage();
+    		break;
+    	case "datetimesort":
+    		fm.sort("datetimesort");
+    		setupPage();
+    		break;
+    		
+    	}
     }
-
+    /* Method that allows to switch the page back.
+     * @param an ActionEvent that represents the back button.
+     */
+    @FXML
+    void turnPageBack(ActionEvent event) {
+    	currentPage -= 1;
+    	setupPage();
+    }
+    /* Method that allows to switch the page forward.
+     * @param an ActionEvent that represents the forward button.
+     */
+    @FXML
+    void turnPageForward(ActionEvent event) {
+    	currentPage += 1;
+    	setupPage();
+    }
+    /* Method that allows to distribute the list in sub lists per page.
+     */
+    public void setupPage() {
+    	int fromIndex = currentPage * ITEMS_PER_PAGE;
+    	int toIndex = Math.min( fromIndex + ITEMS_PER_PAGE, flights.size());
+    	ObservableList<Flight> subFlights = FXCollections.observableArrayList(flights.subList(fromIndex, toIndex));
+    	flightsTable.setItems(subFlights);
+    	if(toIndex < flights.size()) {
+    		forwardButton.setDisable(false);
+    	} else {
+    		forwardButton.setDisable(true);
+    	} if (fromIndex != 0) {
+    		backButton.setDisable(false);
+    	} else {
+    		backButton.setDisable(true);
+    	}
+    }
+    /*
+     * (non-Javadoc)
+     * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
+     */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		currentPage = 0;
 		flights = FXCollections.observableArrayList();
 		fm = new FlightsMonitor(flights);
 		airlineT.setCellValueFactory(new PropertyValueFactory<Flight, String>("airLine"));
@@ -91,6 +218,21 @@ public class FlightsMonitorController implements Initializable{
 		desT.setCellValueFactory(new PropertyValueFactory<Flight, String>("destination"));
 		flightT.setCellValueFactory(new PropertyValueFactory<Flight, Integer>("flightNumber"));
 		timeT.setCellValueFactory(new PropertyValueFactory<Flight, String>("time"));
+		
+		airLineSearch.setUserData("airsearch");
+		boardSearch.setUserData("boardsearch");
+		dateSearch.setUserData("datesearch");
+		desSearch.setUserData("destinationsearch");
+		flightSearch.setUserData("flightsearch");
+		timeSearch.setUserData("timesearch");
+		
+		airLineSort.setUserData("airlinesort");
+		boardSort.setUserData("boardsort");
+		dateSort.setUserData("datesort");
+		datetimeSort.setUserData("datetimesort");
+		desSort.setUserData("dessort");
+		flightSort.setUserData("flightsort");
+		timeSort.setUserData("timesort");
 	}
 
 }
